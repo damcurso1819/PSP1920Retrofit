c07a9678151959b047129531b4c3dbe8e3321aae
cv <damcurso1819@gmail.com>
2019-11-06 09:58:11 +0100

    o

--- app/src/main/java/org/izv/ad/psp1920retrofit/model/Repository.java
+++ app/src/main/java/org/izv/ad/psp1920retrofit/model/Repository.java
@@ -2,6 +2,9 @@ package org.izv.ad.psp1920retrofit.model;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 import org.izv.ad.psp1920retrofit.model.data.Type;
 import org.izv.ad.psp1920retrofit.model.rest.PokemonClient;
 
@@ -20,18 +23,48 @@ public class Repository {
 
     private PokemonClient apiClient;
 
-    private final String url = "54.164.106.27";
+    private String url = "34.205.16.112";
 
-    private List<Type> typeList = new ArrayList<>();;
+    private List<Type> typeList = new ArrayList<>();
+    private MutableLiveData<List<Type>> liveTypeList = new MutableLiveData();
 
     public Repository() {
+       /* Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://" + url + "/web/psp/public/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiClient = retrofit.create(PokemonClient.class);*/
+       retrieveApiClient(url);
+        fetchTypeList();
+    }
+
+    public void add(Type type) {
+        Call<Long> call = apiClient.postType(type);
+        call.enqueue(new Callback<Long>() {
+
+            @Override
+            public void onResponse(Call<Long> call, Response<Long> response) {
+                Log.v(TAG, response.body().toString());
+                long resultado = response.body();
+                if(resultado > 0) {
+                    fetchTypeList();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Long> call, Throwable t) {
+                Log.v(TAG, t.getLocalizedMessage());
+
+            }
+        });
+    }
+
+    private void retrieveApiClient(String url) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://" + url + "/web/psp/public/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
         apiClient = retrofit.create(PokemonClient.class);
-        fetchTypeList();
     }
 
     public void fetchTypeList() {
@@ -42,6 +75,7 @@ public class Repository {
             public void onResponse(Call<ArrayList<Type>> call, Response<ArrayList<Type>> response) {
                 Log.v(TAG, response.body().toString());
                 typeList = response.body();
+                liveTypeList.setValue(response.body());
             }
 
             @Override
@@ -54,5 +88,19 @@ public class Repository {
 
     public List<Type> getTypeList() {
         return typeList;
+    }
+
+    public LiveData<List<Type>> getLiveTypeList() {
+        return liveTypeList;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+        retrieveApiClient(url);
+        /*Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://" + url + "/web/psp/public/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiClient = retrofit.create(PokemonClient.class);*/
     }
 }
